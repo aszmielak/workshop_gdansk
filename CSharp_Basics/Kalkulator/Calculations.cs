c1675a3e9d9eba604399193ac2486506cba94ffc
aszmielak <adam.szmielak@gmail.com>
2019-09-26 12:28:58 +0200

    Dodanie nowej klasy i przeniesienie metod. Dodanie obsługę wychodzenia z aplikacji.

---
+++ CSharp_Basics/Kalkulator/Calculations.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public static class Calculations
+    {
+        /// <summary>
+        /// Method doing all the math
+        /// </summary>
+        /// <param name="numberOne"></param>
+        /// <param name="numberTwo"></param>
+        /// <param name="operation"></param>
+        /// <returns></returns>
+        public static double GetResultValue(int numberOne, int numberTwo, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = Add(numberOne, numberTwo);
+                    break;
+                case "-":
+                    result = Subtract(numberOne, numberTwo);
+                    break;
+                case "x":
+                case "*":
+                    result = Multiply(numberOne, numberTwo);
+                    break;
+                case "/":
+                    if (numberTwo != 0)
+                    {
+                        result = Divide(numberOne, numberTwo);
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Nie dziel przez zero!");
+                    break;
+                default:
+                    Console.WriteLine("Podaj poprawny znak!");
+                    break;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adding two digits
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        private static int Add(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+        /// <summary>
+        /// Substracting two digits
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        private static int Subtract(int firstNumber, int secondNumber)
+        {
+            int sub = firstNumber - secondNumber;
+            return sub;
+        }
+
+        /// <summary>
+        /// Multiplying two digits
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        private static int Multiply(int firstNumber, int secondNumber)
+        {
+            int multi = firstNumber * secondNumber;
+            return multi;
+        }
+
+        /// <summary>
+        /// Dividing two digits
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        private static double Divide(double firstNumber, double secondNumber)
+        {
+            double div = firstNumber / secondNumber;
+            return div;
+        }
+    }
+}
--- CSharp_Basics/Kalkulator/Program.cs
+++ CSharp_Basics/Kalkulator/Program.cs
@@ -12,6 +12,8 @@ namespace Kalkulator
     /// </summary>
     public class Program
     {
+        static bool quit = false;
+
         /// <summary>
         /// Main application
         /// </summary>
@@ -22,16 +24,28 @@ namespace Kalkulator
             //{
             //    Console.WriteLine(j);
             //}
-            bool isRunning = true;
-
-            while (isRunning)
+            while (!quit)
             {
                 Console.WriteLine("Calc");
                 int parsedNumberOne = GetValueFromUser("Wprowadź liczbę a: ");
+                if (quit)
+                {
+                    Console.WriteLine("Zamykam aplikację.");
+                    Console.ReadKey();
+                    return;
+                }
+
                 int parsedNumberTwo = GetValueFromUser("Wprowadź liczbę b: ");
+                if (quit)
+                {
+                    Console.WriteLine("Zamykam aplikację.");
+                    Console.ReadKey();
+                    return;
+                }
+
                 Console.WriteLine("Wprowadź symbol: ");
                 string operation = Console.ReadLine();
-                double finalResult = GetResultValue(parsedNumberOne, parsedNumberTwo, operation);
+                double finalResult = Calculations.GetResultValue(parsedNumberOne, parsedNumberTwo, operation);
 
                 Console.WriteLine($"Wynik działania to: {finalResult}");
                 Console.WriteLine("Kliknij przycisk Q, aby zakończyć...");
@@ -59,103 +73,21 @@ namespace Kalkulator
             while (!parsed)
             {
                 Console.WriteLine(message);
-                string valueFromUser = Console.ReadLine();                
+                string valueFromUser = Console.ReadLine();
+                if (valueFromUser == "q")
+                {
+                    quit = true;
+                    return 0;
+                }
+
                 parsed = int.TryParse(valueFromUser, out parsedValueFromUser);
                 if (!parsed)
                 {
-                    Console.WriteLine($"Nie znana wartość, użyj poprawnej wartości, a nie {parsedValueFromUser}");
-                }
+                    message = ($"Nie znana wartość, użyj poprawnej wartości, a nie {parsedValueFromUser}");
+                }       
             }
 
             return parsedValueFromUser;
-        }
-
-        /// <summary>
-        /// Method doing all the math
-        /// </summary>
-        /// <param name="numberOne"></param>
-        /// <param name="numberTwo"></param>
-        /// <param name="operation"></param>
-        /// <returns></returns>
-        public static double GetResultValue(int numberOne, int numberTwo, string operation)
-        {
-            double result = 0;
-
-            switch (operation)
-            {
-                case "+":
-                    result = Add(numberOne, numberTwo);
-                    break;
-                case "-":
-                    result = Subtract(numberOne, numberTwo);
-                    break;
-                case "x":
-                case "*":
-                    result = Multiply(numberOne, numberTwo);
-                    break;
-                case "/":
-                    if (numberTwo != 0)
-                    {
-                        result = Divide(numberOne, numberTwo);
-                        break;
-                    }
-                    else
-                        Console.WriteLine("Nie dziel przez zero!");
-                    break;
-                default:
-                    Console.WriteLine("Podaj poprawny znak!");
-                    break;
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Adding two digits
-        /// </summary>
-        /// <param name="firstNumber"></param>
-        /// <param name="secondNumber"></param>
-        /// <returns></returns>
-        public static int Add(int firstNumber, int secondNumber)
-        {
-            int sum = firstNumber + secondNumber;
-            return sum;
-        }
-
-        /// <summary>
-        /// substracting two digits
-        /// </summary>
-        /// <param name="firstNumber"></param>
-        /// <param name="secondNumber"></param>
-        /// <returns></returns>
-        public static int Subtract(int firstNumber, int secondNumber)
-        {
-            int sub = firstNumber - secondNumber;
-            return sub;
-        }
-
-        /// <summary>
-        /// Multiplying two digits
-        /// </summary>
-        /// <param name="firstNumber"></param>
-        /// <param name="secondNumber"></param>
-        /// <returns></returns>
-        public static int Multiply(int firstNumber, int secondNumber)
-        {
-            int multi = firstNumber * secondNumber;
-            return multi;
-        }
-
-        /// <summary>
-        /// Dividing two digits
-        /// </summary>
-        /// <param name="firstNumber"></param>
-        /// <param name="secondNumber"></param>
-        /// <returns></returns>
-        public static double Divide(double firstNumber, double secondNumber)
-        {
-            double div = firstNumber / secondNumber;
-            return div;
         }
     }
 }
