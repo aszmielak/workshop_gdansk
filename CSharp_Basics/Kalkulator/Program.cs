05ea110455a80ca0508b15705b9c317177557965
aszmielak <adam.szmielak@gmail.com>
2019-09-26 12:58:53 +0200

    Przeniesienie metody pozyskiwania liczb od użytkownika, a także znaku działania. Poprawki dla quita.

--- CSharp_Basics/Kalkulator/Program.cs
+++ CSharp_Basics/Kalkulator/Program.cs
@@ -12,46 +12,46 @@ namespace Kalkulator
     /// </summary>
     public class Program
     {
-        static bool quit = false;
-
         /// <summary>
         /// Main application
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            //for(int j=0;j<100;j++)
-            //{
-            //    Console.WriteLine(j);
-            //}
-            while (!quit)
+            while (!ValueInterface.Quit)
             {
                 Console.WriteLine("Calc");
-                int parsedNumberOne = GetValueFromUser("Wprowadź liczbę a: ");
-                if (quit)
+                int parsedNumberOne = ValueInterface.GetValueFromUser("Wprowadź liczbę a: ");
+                if (ValueInterface.Quit)
                 {
                     Console.WriteLine("Zamykam aplikację.");
                     Console.ReadKey();
                     return;
                 }
 
-                int parsedNumberTwo = GetValueFromUser("Wprowadź liczbę b: ");
-                if (quit)
+                int parsedNumberTwo = ValueInterface.GetValueFromUser("Wprowadź liczbę b: ");
+                if (ValueInterface.Quit)
                 {
                     Console.WriteLine("Zamykam aplikację.");
                     Console.ReadKey();
                     return;
                 }
 
-                Console.WriteLine("Wprowadź symbol: ");
-                string operation = Console.ReadLine();
+                string operation = ValueInterface.GetOperation();
+                if (ValueInterface.Quit)
+                {
+                    Console.WriteLine("Zamykam aplikację.");
+                    ////Console.ReadKey();
+                    Thread.Sleep(1500);
+                    return;
+                }
                 double finalResult = Calculations.GetResultValue(parsedNumberOne, parsedNumberTwo, operation);
 
                 Console.WriteLine($"Wynik działania to: {finalResult}");
                 Console.WriteLine("Kliknij przycisk Q, aby zakończyć...");
                 string closingOperation = Console.ReadLine();
 
-                if (closingOperation == "q") 
+                if (ValueInterface.Quit) 
                 {
                     Console.WriteLine("Zamykam aplikację.");
                     ////Console.ReadKey();
@@ -59,35 +59,6 @@ namespace Kalkulator
                     return;
                 }
             }
-        }
-
-        /// <summary>
-        /// Get both values from user
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        public static int GetValueFromUser(string message)
-        {
-            bool parsed = false;
-            int parsedValueFromUser = 0;
-            while (!parsed)
-            {
-                Console.WriteLine(message);
-                string valueFromUser = Console.ReadLine();
-                if (valueFromUser == "q")
-                {
-                    quit = true;
-                    return 0;
-                }
-
-                parsed = int.TryParse(valueFromUser, out parsedValueFromUser);
-                if (!parsed)
-                {
-                    message = ($"Nie znana wartość, użyj poprawnej wartości, a nie {parsedValueFromUser}");
-                }       
-            }
-
-            return parsedValueFromUser;
         }
     }
 }
---
+++ CSharp_Basics/Kalkulator/ValueInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public static class ValueInterface
+    {
+        public static bool Quit;
+
+        public static int GetValueFromUser(string message)
+        {
+            bool parsed = false;
+            int parsedValueFromUser = 0;
+            while (!parsed)
+            {
+                Console.WriteLine(message);
+                string valueFromUser = Console.ReadLine();
+                if (valueFromUser == "q")
+                {
+                    Quit = true;
+                    return 0;
+                }
+
+                parsed = int.TryParse(valueFromUser, out parsedValueFromUser);
+                if (!parsed)
+                {
+                    message = ($"Podana wartość - {valueFromUser} - jest niepoprawna, podaj poprawną:");
+                }
+            }
+
+            return parsedValueFromUser;
+        }
+
+        public static string GetOperation()
+        {
+            Console.WriteLine("Podaj operację: ");
+            string operation = Console.ReadLine();
+            if (operation == "q")
+            {
+                Quit = true;
+                return string.Empty;
+            }
+            return operation;
+        }
+    }
+}
