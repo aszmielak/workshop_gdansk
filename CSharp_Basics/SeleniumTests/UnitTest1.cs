f7a02ca880898cd96bdc2128f25eeebd0f2a02e3
aszmielak <adam.szmielak@gmail.com>
2019-09-27 08:46:20 +0200

    Pierwszy test Selenium.

---
+++ CSharp_Basics/SeleniumTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            ////konstruktor klasy drivera
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void FirstTest()  ////metoda
+        {
+            Assert.AreEqual(2, 1);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
