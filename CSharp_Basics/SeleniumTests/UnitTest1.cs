fa204e0a33611745846be18b224f852680e6f4f8
aszmielak <adam.szmielak@gmail.com>
2019-09-27 10:16:19 +0200

    Pierwsze trzy testy na stronie testowej o sukienkach. Przekierowanie do kategorii i policzenie produktów.

--- CSharp_Basics/SeleniumTests/UnitTest1.cs
+++ CSharp_Basics/SeleniumTests/UnitTest1.cs
@@ -15,18 +15,52 @@ namespace SeleniumTests
         {
             ////konstruktor klasy drivera
             driver = new ChromeDriver();
+            ////gra do zabawy css selectorów: https://flukeout.github.io/#
+            ////wtyczka do Chroma "Web Developer" koło zebate ikonka
+            var waitTime = new TimeSpan(0, 0, 1);
+            driver.Manage().Timeouts().ImplicitWait = waitTime;
         }
 
         [Test]
         public void FirstTest()  ////metoda
         {
-            Assert.AreEqual(2, 1);
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li> a[title='Dresses']"));
+            categoryHeader.Click();
+            var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productCounter.Text;
+
+            StringAssert.Contains("5", text);
+        }
+
+        [Test]
+        public void SecondTest()  ////metoda
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li> a[title='Women']"));
+            categoryHeader.Click();
+            var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productCounter.Text;
+
+            StringAssert.Contains("7", text);
+        }
+
+        [Test]
+        public void ThirdTest()  ////metoda
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li> a[title='T-shirts']"));
+            categoryHeader.Click();
+            var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productCounter.Text;
+
+            StringAssert.Contains("1", text);
         }
 
         [TearDown]
         public void TearDown()
         {
-            driver.Quit();
+           driver.Quit();
         }
     }
 }
